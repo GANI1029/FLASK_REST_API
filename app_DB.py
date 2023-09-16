46ee090f25f4c769faa2967a4dbc78c99902c1db
GANESH RAJA MANDALA <ganeshrajamandala@gmail.com>
2023-09-16 23:58:47 +0530

    final commit

--- app_DB.py
+++ app_DB.py
@@ -32,6 +32,15 @@ user_put_ars.add_argument('user_city', type = str , required=True ,   help="erro
 user_put_ars.add_argument('email', type = str , required=True ,   help="error user  email required ")
 user_put_ars.add_argument('phone', type = int , required=True ,   help="error user  phone required ")
 
+# using to update user data 
+user_update_ars = reqparse.RequestParser()
+user_update_ars.add_argument('user_name', type = str , required=False ,  help="error user name required ")
+user_update_ars.add_argument('user_age', type = int , required=False ,   help="error user_age required ")
+user_update_ars.add_argument('user_gender', type = str , required=False , help="error user_gender required ")
+user_update_ars.add_argument('user_city', type = str , required=False ,   help="error user_city required ")
+user_update_ars.add_argument('email', type = str , required=False ,   help="error user  email required ")
+user_update_ars.add_argument('phone', type = int , required=False ,   help="error user  phone required ")
+
 user_fields = {
     'id' : fields.Integer ,
     'user_name': fields.String,
@@ -45,15 +54,18 @@ class user(Resource):
     @marshal_with(user_fields)
     def get(self ,ID):
         result  = Userdetails.query.filter_by(id = ID).first()
+        if not result:
+            abort(403 , message = 'could not find User ID...')
+
         return result
     
     @marshal_with(user_fields)
     def put(self ,ID):
         args = user_put_ars.parse_args()
         result  = Userdetails.query.filter_by(id = ID).first()
-        
+
         if result:
-            abort(409 , message = 'User ID taken...Use Different ID')
+            abort(409 , message = 'User ID taken...Use Different ID...')
 
         user = Userdetails(id = ID ,
                             user_name =args['user_name'] ,
@@ -65,6 +77,40 @@ class user(Resource):
         db.session.add(user)
         db.session.commit()
         return user
+    
+    @marshal_with(user_fields)
+    def patch(self ,ID):
+        args = user_update_ars.parse_args()
+        result  = Userdetails.query.filter_by(id = ID).first()
+
+        if not result:
+            abort(404 , message = "User ID doesn't exist... please use valid user id to update user details ")
+
+        # Define a list of argument names to consider for updating
+        updateable_fields = ['user_name', 'user_age', 'user_gender', 'user_city', 'email', 'phone']
+
+        # Iterate through the argument names and update the Userdetails object if the argument is provided
+        for field_name in updateable_fields:
+            if args[field_name] is not None:
+                setattr(result, field_name, args[field_name])
+
+        db.session.commit()
+        return result
+    
+    def delete(self, ID):
+        result = Userdetails.query.filter_by(id=ID).first()
+
+        if not result:
+            abort(404, message="User ID doesn't exist... please use a valid user ID to delete the user")
+
+        # Delete the user from the database
+        db.session.delete(result)
+        db.session.commit()
+
+        return {'message': 'User deleted successfully', 'user id' : ID , 'user name':result.user_name }, 200  # 204 means No Content
+
+
+
     
 api.add_resource(user,'/user/<int:ID>')
 
